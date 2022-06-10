c67afb05cc2c3b3882306d7165ad5785fc6bb25f
carlo-ddm <carloduminuco90@gmail.com>
2022-06-10 11:09:47 +0200

    esercizio completo! migliorare tag lista

--- index.php
+++ index.php
@@ -38,17 +38,26 @@ $faqs = [
     "domanda" => "Perché il mio account è associato a un paese?",
     "risposta" => "<p>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
     </p>
+
     <ol>
-    <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+
+    <li>
+    La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
       <ol type='a'>
-      <li> Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera. <li/>
-      <li> Google LLC, con sede negli Stati Uniti, per il resto del mondo.  <li/>
+        <li>
+          Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+        <li/>
+        <li>
+          Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+        <li/>
       <ol/>
       <li/>
    
-    La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
-    <ol/>
-    </p><p>
+    <li>
+      La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+    </li>
+      <ol/>
+      </p><p>
     Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
     
     <p class='sottotitolo'>Stabilire il paese associato al tuo account
